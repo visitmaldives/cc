6c6d73e3cce49bac6d55836993161d79690cf47c
Zameer <xameeer@gmail.com>
2025-07-08 14:07:39 +0500

    Feat: User Authentication Using session data

--- app/Auth/SessionUser.php
+++ app/Auth/SessionUser.php
@@ -4,7 +4,7 @@ namespace App\Auth;
 
 use Illuminate\Contracts\Auth\Authenticatable;
 
-class SessionUser implements Authenticicatable
+class SessionUser implements Authenticatable
 {
     protected $attributes;
 
