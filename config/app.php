151da4ea82d29f2d4ca7542b873940666dc7fc57
Zameer <xameeer@gmail.com>
2025-07-08 10:45:07 +0500

    Feat: User Authentication Using session data

--- config/app.php
+++ config/app.php
@@ -123,4 +123,10 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'providers' => [
+        // ...
+        App\Providers\AuthServiceProvider::class,
+        // ...
+    ],
+
 ];
