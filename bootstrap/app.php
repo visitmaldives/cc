8270abad0515612f998ee6cc3ef798cf6cfaeda6
Zameer <xameeer@gmail.com>
2025-07-08 11:09:56 +0500

    Feat: User Authentication Using session data

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->withProviders([
+        App\Providers\AuthServiceProvider::class,
+    ])
+    ->create();
